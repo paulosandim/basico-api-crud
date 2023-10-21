9a858642b38b9f209ec5bdce5270045134e5f38b
Paulo Sandim <paulosandim@moss.earth>
2023-10-21 10:55:23 -0300

    metodo get da api com return

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function(request, response) {
-  return response.json({
-    message: 'olá, dev!'
-  })
+app.get('/projects', function(request, response) {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2'
+  ])
 })
 
 app.listen(3000, () => {
